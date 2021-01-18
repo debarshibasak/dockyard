1559e97a574d3428f3bd3281761483d41f6f3e34
debarshibasak <debarshri@gmail.com>
2021-01-18 01:41:40 +0100

    CGO added to the build

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 go build -o dockyard_macos cmd/main.go
-GOOS=linux go build -o dockyard_linux cmd/main.go
+CGO_ENABLED=0 GOOS=linux go build -o dockyard_linux cmd/main.go
 GOOS=windows go build -o dockyard.exe cmd/main.go
